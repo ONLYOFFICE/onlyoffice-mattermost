e17d6a961fc259b8118f75514c8b72a1b0b7fa5f
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2026-06-03 17:39:38 +0500

    fix: move liveview to common config

--- server/web/common/config.go
+++ server/web/common/config.go
@@ -142,6 +142,33 @@ func BuildEditorConfig(
 		theme = "default-dark"
 	}
 
+	editorConfig := oomodel.EditorConfig{
+		User: oomodel.User{
+			ID:    payload.UserID,
+			Name:  payload.Username,
+			Image: fmt.Sprintf("%s/image?code=%s", serverURL, code),
+		},
+		CallbackURL: serverURL + "/callback?file=" + payload.FileID,
+		Customization: oomodel.Customization{
+			UiTheme: theme,
+			Close: oomodel.Close{
+				Visible: true,
+			},
+			Plugins: configuration.PluginsEnabled,
+			Macros:  configuration.MacrosEnabled,
+		},
+		Lang: payload.Lang,
+	}
+
+	editorConfig.CoEditing = oomodel.CoEditing{
+		Mode:   "fast",
+		Change: permissions.Edit,
+	}
+
+	if !permissions.Edit {
+		editorConfig.Mode = "view"
+	}
+
 	config = oomodel.Config{
 		Document: oomodel.Document{
 			FileType:    fileInfo.Extension,
@@ -151,24 +178,8 @@ func BuildEditorConfig(
 			Permissions: permissions,
 		},
 		DocumentType: docType,
-		EditorConfig: oomodel.EditorConfig{
-			User: oomodel.User{
-				ID:    payload.UserID,
-				Name:  payload.Username,
-				Image: fmt.Sprintf("%s/image?code=%s", serverURL, code),
-			},
-			CallbackURL: serverURL + "/callback?file=" + payload.FileID,
-			Customization: oomodel.Customization{
-				UiTheme: theme,
-				Close: oomodel.Close{
-					Visible: true,
-				},
-				Plugins: configuration.PluginsEnabled,
-				Macros:  configuration.MacrosEnabled,
-			},
-			Lang: payload.Lang,
-		},
-		Type: tools.IsMobile(r.Header.Get("User-Agent")),
+		EditorConfig: editorConfig,
+		Type:         tools.IsMobile(r.Header.Get("User-Agent")),
 	}
 
 	config.IssuedAt, config.ExpiresAt = jwt.NewNumericDate(time.Now()),
